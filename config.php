9592bd637c57468dd3fd477098aa9919969fc440
MARK <37331833+iimarks@users.noreply.github.com>
2018-05-31 02:05:59 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,3 +1,9 @@
 <?php
+$TOKEN = ""; // bot token
+$botUsername = ""; // bot username without @
 
-
+$databaseHost = "localhost";
+$databaseName = ""; // db name
+$databaseUsername = ""; // db username
+$databasePassword = ""; // db password
+$sudoID = ""; // sudo id
