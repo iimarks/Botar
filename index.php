02388556fec34eaacc7488170735610a3733ca18
MARK <37331833+iimarks@users.noreply.github.com>
2018-06-01 07:18:31 +0300

    Update index.php

--- index.php
+++ index.php
@@ -19,6 +19,28 @@ $chatId = $messageData["chat"]["id"];
 $messageId = $messageData["message_id"];
 $messageText = $messageData["text"];
 
+$data = $updateData["callback_query"]["data"];
+$from_id = $messageData["from"]["id"];
+$from_name = $messageData["from"]["first_name"] . $messageData["from"]["last_name"];
+$from_username = $messageData["from"]["username"];
+
+// media
+$sticker = $messageData["sticker"];
+$sticker_id = $messageData["sticker"]["file_id"];
+$voice = $messageData["voice"];
+$voice_id = $messageData["voice"]["file_id"];
+$file = $messageData["document"];
+$file_id = $messageData["document"]["file_id"];
+$audio = $messageData["audio"];
+$audio_id = $messageData["audio"]["file_id"];
+$video = $messageData["video"];
+$video_id = $messageData["video"]["file_id"];
+$contact = $messageData["contact"];
+$contact_id = $messageData["contact"]["file_id"];
+$photo = $messageData["photo"];
+$photo_id = $messageData["message"]["photo"][0]["file_id"];
+
+
 if(isset($messageData["text"])){
     //if message came late more than 15 second, exit
     if($messageTime+15 < time()){
