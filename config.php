c58b14fe2c01a64053b25ab8382f59c17e589711
MARK <37331833+iimarks@users.noreply.github.com>
2018-06-02 04:38:48 +0300

    Update config.php

--- config.php
+++ config.php
@@ -2,10 +2,13 @@
 $TOKEN = ""; // bot token
 $botUsername = ""; // bot username without @
 
+/*
 $databaseHost = "localhost";
 $databaseName = ""; // db name
 $databaseUsername = ""; // db username
 $databasePassword = ""; // db password
+*/
+
 $sudoID = ""; // sudo id
 
 date_default_timezone_set("Asia/Riyadh");
